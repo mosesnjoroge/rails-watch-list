87f5037a40a6ae33c03596eed9b79a6bea0c07dc
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-10-24 12:02:45 -0400

    all tests past-debug

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,37 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: %i[new create]
+  before_action :set_bookmark, only: %i[destroy]
+  before_action :set_list, only: %i[new create]
+
   def new
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    # @bookmark.list = @list
-    # if @bookmark.save
-    #   redirect_to list_path(@list), notice: 'bookmark was successfully created'
-    # else
-    #   render :new, status: :see_other
-    # end
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   def destroy
     @bookmark.destroy
-    # redirect_to bookmarks_path
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
   def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
     @list = List.find(params[:list_id])
   end
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
-  validates :comment, presence: true, uniqueness: true, length: { minimum: 6 }
-  validates :movie, presence: true
-  validates :list, presence: true
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
