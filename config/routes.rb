0fd26a749335e7f0e59876bd664ca464545f8e1a
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-10-27 20:57:51 -0400

    fred code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: 'lists#index'
-  resources :lists, only: %i[new create show] do
+  resources :lists, only: %i[index new create show] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: [:destroy]
